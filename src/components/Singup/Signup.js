e2e90c32cff9889fc6702d82a6e1e32ce59db8ee
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2023-12-17 11:10:03 +0530

    made form fillable

--- src/components/Singup/Signup.js
+++ src/components/Singup/Signup.js
@@ -7,31 +7,27 @@ const Signup = () => {
         <div className="div">
           <img className="img" alt="Group" src="https://i.ibb.co/1d5Hxrw/Screenshot-429.png" />
           <div className="overlap-group-wrapper">
-            <div className="overlap-group">
+            <button className="overlap-group">
               <img className="flat-color-icons" alt="Flat color icons" src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg" />
               <div className="text-wrapper">Continue with Google</div>
-            </div>
+            </button>
           </div>
           <div className="group-2">
             <img className="line" alt="Line" src="https://i.ibb.co/XVdRT8m/6f620273-ca77-4e0e-be61-e5bd5b5847c9.png" />
             <div className="text-wrapper-2">Or</div>
             <img className="line-2" alt="Line" src="https://i.ibb.co/XVdRT8m/6f620273-ca77-4e0e-be61-e5bd5b5847c9.png" />
           </div>
-          <div className="overlap-wrapper">
-            <div className="overlap">
-              <div className="text-wrapper-3">Email</div>
-            </div>
-          </div>
-          <div className="div-wrapper">
-            <div className="overlap">
-              <div className="text-wrapper-4">Password</div>
-            </div>
-          </div>
-          <div className="group-3">
+          <input style={{backgroundColor:"#f8f9f7"}} className="px-2 overlap-wrapper overlap"
+            placeholder="Email"
+          />
+          <input style={{backgroundColor:"#f8f9f7"}} className="px-2 div-wrapper overlap"
+            placeholder="Password"
+          />
+          <button className="group-3">
             <div className="overlap-2">
               <div className="text-wrapper-5">Continue</div>
             </div>
-          </div>
+          </button>
           <p className="by-creating-an">
             <span className="span">By creating an account, you are agreeing to our</span>
             <span className="text-wrapper-6">&nbsp;</span>
@@ -43,7 +39,7 @@ const Signup = () => {
           </p>
           <div className="group-4">
             <div className="text-wrapper-8">Already have an account?</div>
-            <div className="text-wrapper-9">Login</div>
+            <button className="text-wrapper-9">Login</button>
           </div>
         </div>
       </div>

8ab31a9183f9da9a37be4a61c19f11b5095cbf0f
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2024-02-12 02:08:13 +0530

    connected api to featured

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Stack, Typography } from "@mui/material";
 import TrendingCard from './TrendingCard';
 
 const Trending = () => {
+    const location = "Lucknow"
+    const [upComing, setUpComing] = useState([])
+
+    const getUpcoming = async () => {
+        const response = await fetch(`http://localhost:5000/api/properties/upcoming`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": localStorage.getItem("token")
+            },
+            body: JSON.stringify({ location })
+        });
+        const json = await response.json()
+        console.log(json)
+        setUpComing(json)
+    }
+
+    useEffect(() => {
+        getUpcoming()
+    }, [location])
 
     return (
         <>
             <div style={{ marginTop: "90px" }} className='container'><Typography variant="h4" fontWeight={"bold"} mb={2} sx={{ color: "#1A363E", textAlign: "left", width: "100%" }}>
-                Trending Projects
+                Up-Coming Projects
             </Typography></div>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-md-4'>
-                        <TrendingCard />
-                    </div>
-                    <div className='col-md-4'>
-                        <TrendingCard />
-                    </div>
-                    <div className='col-md-4'>
-                        <TrendingCard />
-                    </div>
-                    <div className='col-md-4'>
-                        <TrendingCard />
-                    </div>
-                    <div className='col-md-4'>
-                        <TrendingCard />
-                    </div>
-                    <div className='col-md-4'>
-                        <TrendingCard />
-                    </div>
+                    {upComing.map(upcomingitem => {
+                        return (
+                            <div className='col-md-4'>
+                                <TrendingCard upcomingitem={upcomingitem} />
+                            </div>
+                        )
+                    })}
+
                 </div>
             </div>
 
--- src/components/Trending/TrendingCard.js
+++ src/components/Trending/TrendingCard.js
@@ -1,20 +1,20 @@
 import { Height } from '@mui/icons-material'
 import React from 'react'
 
-const TrendingCard = () => {
+const TrendingCard = ({ upcomingitem }) => {
     return (
-        <div class="card mb-3" style={{maxWidth: "540px" ,boxShadow: "0px 4px 10px 5px #0000001a",borderRadius:"10px",border:"none"}}>
+        <div class="card mb-3" style={{ maxWidth: "540px", boxShadow: "0px 4px 10px 5px #0000001a", borderRadius: "10px", border: "none" }}>
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img style={{height:"100%"}} src="https://i.ibb.co/NxLH2Ph/Whats-App-Image-2024-01-21-at-01-15-34.jpg" class="img-fluid rounded-start" alt="..." />
+                    <img style={{ height: "100%" }} src="https://i.ibb.co/NxLH2Ph/Whats-App-Image-2024-01-21-at-01-15-34.jpg" class="img-fluid rounded-start" alt="..." />
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <p style={{fontSize:"20px",color:"#1A363E"}} class="card-title">Feet Skyvillie</p>
-                        <p style={{fontSize:"12px",color:"#939393",fontWeight:"500",letterSpacing: "0",lineHeight: "normal"}}  class="card-text">By M/S SUNITA CONSTRUCTION CO</p>
-                        <p style={{fontSize:"12px",color:"#45666B",fontWeight:"500",letterSpacing: "0",lineHeight: "normal"}} class="card-text mt-2">3BHK Apartment</p>
-                        <p style={{fontSize:"12px",fontWeight:"500",color:"#939393",letterSpacing: "0",lineHeight: "normal"}} class="card-text mt-2">Khasra No. 99/2/1, 99/2/2, 100/1 And 100/2, Ayodhya Bypass, Bhopal</p>
-                        <p style={{fontSize:"12px",fontWeight:"500",color:"#1A363E",letterSpacing: "0",lineHeight: "normal"}} class="card-text mt-2">56 Lacs.</p>
+                        <p style={{ fontSize: "20px", color: "#1A363E" }} class="card-title">{upcomingitem.title}</p>
+                        <p style={{ fontSize: "12px", color: "#939393", fontWeight: "500", letterSpacing: "0", lineHeight: "normal" }} class="card-text">By {upcomingitem.owner}</p>
+                        <p style={{ fontSize: "12px", color: "#45666B", fontWeight: "500", letterSpacing: "0", lineHeight: "normal" }} class="card-text mt-2">{upcomingitem.listing_type}</p>
+                        <p style={{ fontSize: "12px", fontWeight: "500", color: "#939393", letterSpacing: "0", lineHeight: "normal" }} class="card-text mt-2">{upcomingitem.address}</p>
+                        <p style={{ fontSize: "12px", fontWeight: "500", color: "#1A363E", letterSpacing: "0", lineHeight: "normal" }} class="card-text mt-2">{upcomingitem.price} Lacs.</p>
                     </div>
                 </div>
             </div>
